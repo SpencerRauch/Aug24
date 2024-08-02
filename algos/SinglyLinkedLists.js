f5305cb097c7db513ee7d0e516cb97f5626e3a76
Spencer Rauch <rauchs@gmail.com>
2024-08-02 08:37:05 -0700

    solutions

--- algos/SinglyLinkedLists.js
+++ algos/SinglyLinkedLists.js
@@ -298,7 +298,18 @@ class SinglyLinkedList {
     * @returns {SinglyLinkedList} This list with the added nodes.
     */
     concat(addList) {
-        //Your code here
+        if (this.isEmpty()) {
+            this.head = addList.head;
+            return this;
+        }
+
+        let runner = this.head;
+        while (runner.next) {
+            runner = runner.next;
+        }
+        runner.next = addList.head;
+
+        return this;
     }
 
     /**
@@ -308,7 +319,34 @@ class SinglyLinkedList {
     * @returns {SinglyLinkedList} This list.
     */
     reverse() {
-        //Your code here
+        if (!this.head || !this.head.next) {
+            return this;
+        }
+    
+        let current = this.head;
+    
+        while (current.next) {
+            const newHead = current.next;
+            // cut the newHead out from where it currently is
+            current.next = current.next.next;
+            newHead.next = this.head;
+            this.head = newHead;
+        }
+        return this;
+    }
+
+    reverse2() {
+        let prev = null;
+        let node = this.head;
+    
+        while (node) {
+            const nextNode = node.next;
+            node.next = prev;
+            prev = node;
+            node = nextNode;
+        }
+        this.head = prev;
+        return this;
     }
 
 }
